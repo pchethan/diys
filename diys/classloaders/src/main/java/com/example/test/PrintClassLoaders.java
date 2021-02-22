b0641509b085d0683b227c5cd59678ac5a0e6c79
Chethan <pchethan@gmail.com>
2021-02-23 01:51:31 +0530

    added diagrams

--- diys/classloaders/src/main/java/com/example/test/PrintClassLoaders.java
+++ diys/classloaders/src/main/java/com/example/test/PrintClassLoaders.java
@@ -4,7 +4,8 @@ import java.util.Optional;
 
 public class PrintClassLoaders
 {
-    public static void main( String[] args )  {
+    public static void main( String[] args ) throws InterruptedException {
         Utils.printCLs(PrintClassLoaders.class, (str) -> System.out.println(str));
+        Thread.sleep(1000000l);
     }
 }
